6d7434adab361a01a1d823fe73f3fcd3218d776b
3dju <leveljub@gmail.com>
2018-11-15 15:35:21 -0200

    implementing photon, adding basic launcher logic

---
+++ Puppet/Assets/Source/Network/Launcher.cs
@@ -0,0 +1,101 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puppet.Network.Launcher
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+
+        #region Private Serializable Variables
+
+        #endregion
+
+        #region Private Variables
+        /// <sumary>
+        /// This client's version number. Users are separated from each other by gameversion. (which allows you to make breaking changes).
+        /// </sumary>
+        private string _gameVersion = "1";
+
+        /// <summary>
+        /// The maximum number of players per room. If maximum is reached, will create a new room.
+        /// </summary>
+        [SerializeField]
+        private byte _maxPlayersPerRoom = 4;
+
+        #endregion
+
+        #region MonoBehaviourPunCallbacks Callbacks
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Connected!");
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("Joined room failed! Error: " + returnCode + " " + message);
+            Debug.Log("Will create room.");
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = _maxPlayersPerRoom });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Joined room!");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.Log("Disconnected because: " + cause);
+        }
+
+        /// <sumary>
+        /// Monobehaviour method called on early initialization
+        /// </sumary>
+        private void Awake()
+        {
+            // # Critical
+            // We don't join the lobby. There's no need to join the loby to get the list of rooms.
+            PhotonNetwork.AutomaticallySyncScene = true;
+            
+        }
+
+        ///<summary>
+        /// Monobehaviour called during initialization
+        ///</summary>
+        
+        private void Start()
+        {
+            Connect();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+        /// <summary>
+        /// Start the connection process.
+        /// - If already connected, we attempt joining a random room.
+        /// - If not yet connected, Connect this application instance to Photon Cloud Network
+        /// </summary>
+        public void Connect()
+        {
+            if(PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.GameVersion = _gameVersion;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+
+        #endregion
+    }
+
+}
